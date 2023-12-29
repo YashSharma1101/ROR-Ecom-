9b909dc36972f85177b6c3f0f012757b60f4f22f
YashSharma1101 <yashsharma0787@gmail.com>
2023-12-29 17:32:25 +0530

    Updated dependencies and made other changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   
   devise_for :users, controllers: { confirmations: 'users/confirmations', 
     omniauth_callbacks: 'users/omniauth_callbacks',
-    registrations: 'registrations', sessions: 'users/sessions', sessions: 'sessions'
+    registrations: 'registrations', sessions: 'users/sessions'#, sessions: 'sessions'
    }
   
   devise_scope :user do
