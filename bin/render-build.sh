99fec4d4ea196dc5c6a3047b80667fbec081d947
YashSharma1101 <yashsharma0787@gmail.com>
2024-01-04 15:48:49 +0530

    changes in gem

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
-bundle install
-./bin/rails assets:precompile
-./bin/rails assets:clean
+bundle install --no-deployment
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
+bundle exec rails db:seed
